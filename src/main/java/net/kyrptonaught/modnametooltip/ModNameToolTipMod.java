1bdee347a150b09672ad2cf661f8fd85ebbc8773
kyrptonaught <anthony.butillo10@gmail.com>
2019-08-17 18:21:19 -0400

    Clean up

--- src/main/java/net/kyrptonaught/modnametooltip/ModNameToolTipMod.java
+++ src/main/java/net/kyrptonaught/modnametooltip/ModNameToolTipMod.java
@@ -1,19 +1,10 @@
 package net.kyrptonaught.modnametooltip;
 
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
-
 import java.util.HashMap;
 
 
-public class ModNameToolTipMod implements ModInitializer, ClientModInitializer {
-	public static final String MOD_ID = "modnameitemtool";
-	public static HashMap<String,String> modCache = new HashMap<>();
-	@Override
-	public void onInitialize() {
-	}
+public class ModNameToolTipMod {
+    public static final String MOD_ID = "modnameitemtool";
+    public static HashMap<String, String> modCache = new HashMap<>();
 
-	@Override
-	public void onInitializeClient() {
-	}
 }
--- src/main/java/net/kyrptonaught/modnametooltip/mixin/ItemMixin.java
+++ src/main/java/net/kyrptonaught/modnametooltip/mixin/ItemMixin.java
@@ -23,26 +23,24 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-
 import java.util.List;
 
 @Mixin(ItemStack.class)
 public class ItemMixin {
 
-	@Shadow
-	@Final
-	private Item item;
+    @Shadow
+    @Final
+    private Item item;
 
-	@Inject(at = @At("TAIL"), method = "getTooltip")
-	@Environment(EnvType.CLIENT)
-	public List<Text> MNTT$getTooltip(PlayerEntity playerEntity_1, TooltipContext tooltipContext_1, CallbackInfoReturnable callbackInfoReturnable) {
-		List<Text> list = (List<Text>) callbackInfoReturnable.getReturnValue();
-		String mod = Registry.ITEM.getId(item).getNamespace();
-		if (!ModNameToolTipMod.modCache.containsKey(mod)) {
-			ModNameToolTipMod.modCache.put(mod, StringUtils.capitalize(FabricLoader.getInstance().getModContainer(mod).map(ModContainer::getMetadata).map(ModMetadata::getName).orElse(mod)));
-		}
-		mod = ModNameToolTipMod.modCache.get(mod);
-		list.add(new TranslatableText(mod).formatted(Formatting.BLUE, Formatting.ITALIC));
-		return list;
-	}
+    @Inject(at = @At("TAIL"), method = "getTooltip")
+    @Environment(EnvType.CLIENT)
+    private void MNTT$getTooltip(PlayerEntity playerEntity_1, TooltipContext tooltipContext_1, CallbackInfoReturnable callbackInfoReturnable) {
+        List<Text> list = (List<Text>) callbackInfoReturnable.getReturnValue();
+        String mod = Registry.ITEM.getId(item).getNamespace();
+        if (!ModNameToolTipMod.modCache.containsKey(mod)) {
+            ModNameToolTipMod.modCache.put(mod, StringUtils.capitalize(FabricLoader.getInstance().getModContainer(mod).map(ModContainer::getMetadata).map(ModMetadata::getName).orElse(mod)));
+        }
+        mod = ModNameToolTipMod.modCache.get(mod);
+        list.add(new TranslatableText(mod).formatted(Formatting.BLUE, Formatting.ITALIC));
+    }
 }
